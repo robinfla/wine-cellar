11dfdcf1a31f7f683f4a2f3a93f9ccf668bf29de
Robin Flamant <rob.flamant@gmail.com>
2026-01-21 20:16:02 +0100

    Fix reference-data: sequential queries to avoid connection pool exhaustion

--- server/api/inventory/import/reference-data.get.ts
+++ server/api/inventory/import/reference-data.get.ts
@@ -8,15 +8,12 @@ export default defineEventHandler(async (event) => {
     throw createError({ statusCode: 401, message: 'Unauthorized' })
   }
 
-  const [allCellars, allRegions, allAppellations, allGrapes, allFormats, existingHashes] =
-    await Promise.all([
-      db.select({ id: cellars.id, name: cellars.name }).from(cellars).where(eq(cellars.userId, userId)),
-      db.select({ id: regions.id, name: regions.name }).from(regions),
-      db.select({ id: appellations.id, name: appellations.name }).from(appellations),
-      db.select({ id: grapes.id, name: grapes.name }).from(grapes),
-      db.select({ id: formats.id, name: formats.name, volumeMl: formats.volumeMl }).from(formats),
-      db.select({ hash: inventoryLots.importHash }).from(inventoryLots).where(eq(inventoryLots.userId, userId)),
-    ])
+  const allCellars = await db.select({ id: cellars.id, name: cellars.name }).from(cellars).where(eq(cellars.userId, userId))
+  const allRegions = await db.select({ id: regions.id, name: regions.name }).from(regions)
+  const allAppellations = await db.select({ id: appellations.id, name: appellations.name }).from(appellations)
+  const allGrapes = await db.select({ id: grapes.id, name: grapes.name }).from(grapes)
+  const allFormats = await db.select({ id: formats.id, name: formats.name, volumeMl: formats.volumeMl }).from(formats)
+  const existingHashes = await db.select({ hash: inventoryLots.importHash }).from(inventoryLots).where(eq(inventoryLots.userId, userId))
 
   return {
     cellars: allCellars,
